0bd1d0c99088ee735b62229781b5ce487977a65b
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-03-25 11:04:14 +0100

    Add styling solution
    Signed-off-by: Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>

--- frontend/src/Components/Results.ts
+++ frontend/src/Components/Results.ts
@@ -1,6 +1,15 @@
 import {html} from "../html.js";
 import {WebSocketConsumer} from "./WebSocket.js";
+import {css} from "../css.js";
+
+const styling=css`
+  background: yellow;
+  
+  .button {
+    color: red
+  }
+`;
 
 export const Results = () => html`<${WebSocketConsumer}>${
-    value => html`<div>We got: ${JSON.stringify(value.state)}</div>`
+    value => html`<div className=${styling}><span className="button">We</span> got: ${JSON.stringify(value.state)}</div>`
 }<//>`;
---
+++ frontend/src/css.ts
@@ -0,0 +1,1 @@
+export {default as css} from '../node_modules/csz/index.js';
