c77d3956009be71e19e0f066f6af52ff0daa9315
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-03-25 11:09:41 +0100

    Extract types

--- frontend/src/Components/WebSocket.ts
+++ frontend/src/Components/WebSocket.ts
@@ -1,24 +1,8 @@
 import {React} from '../../node_modules/es-react/dev/index.js';
 import {html} from '../html.js';
+import {CardValue, WebSocketApi} from "../types/WebSocket.js";
 
 const WebSocketContext = React.createContext('defaultValue');
-
-type CardValue = 'not-voted' | 'coffee' | '3';
-
-interface WebSocketState {
-    resultsVisible: boolean,
-    votes: {
-        [userId: string]: CardValue
-    }
-}
-
-interface WebSocketApi {
-    state: WebSocketState,
-
-    setVote(vote: CardValue): void
-    revealVotes(): void,
-    resetVotes(): void
-}
 
 export const WebSocketProvider = ({children}) => {
     const value: WebSocketApi = {
