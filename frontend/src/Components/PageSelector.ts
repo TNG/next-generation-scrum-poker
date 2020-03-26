09be32cd1114bea59e1c1523713e7c44f46e8654
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-03-26 14:29:16 +0100

    Improve PageSelector

--- frontend/src/Components/PageSelector.ts
+++ frontend/src/Components/PageSelector.ts
@@ -1,4 +1,7 @@
 import { html } from '../html.js';
+import { WebSocketConsumer } from './WebSocket.js';
+import { WebSocketApi } from '../types/WebSocket.js';
 import { VotingPage } from './VotingPage.js';
 
-export const PageSelector = () => html`<${VotingPage} />`;
+export const PageSelector = () =>
+  html`<${WebSocketConsumer}>${(value: WebSocketApi) => html`<${VotingPage} />`}<//>`;
