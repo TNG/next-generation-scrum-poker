e82b6b0114352bafd6ae2a1fb2bc16bd818bf374
Edgar Kellermann <edgar.kellermann@tngtech.com>
2020-03-26 15:10:56 +0100

    send vote request when card is clicked
    Signed-off-by: Edgar Kellermann <edgar.kellermann@tngtech.com>

--- frontend/src/Components/VotingPage.ts
+++ frontend/src/Components/VotingPage.ts
@@ -1,33 +1,45 @@
 import { html } from '../html.js';
-import { CardValue } from "../types/WebSocket.js";
-import {css} from "../css.js";
+import { CardValue } from '../types/WebSocket.js';
+import { css } from '../css.js';
+import { WebSocketConsumer } from './WebSocket.js';
 
-const card=css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-width: 3px;
-    border-style: solid;
-    border-color: black;
-    border-radius: 10px;
-    height: 200px;
-    width:100px;
-    margin: 10px;
-    :hover {
-      background: #e1e1e1;
-    }
-    :active {
-      background: green;
-    }
+const card = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-width: 3px;
+  border-style: solid;
+  border-color: black;
+  border-radius: 10px;
+  height: 200px;
+  width: 100px;
+  margin: 10px;
+  :hover {
+    background: #e1e1e1;
+  }
+  :active {
+    background: green;
+  }
 `;
-const CARD_VALUES: Array<CardValue> = ['coffee', '1', '2', '3', '5', '8', '13', '20']
-const Card = cardValue => html`<div className=${card}>${cardValue}</div>`;
+const CARD_VALUES: Array<CardValue> = ['coffee', '1', '2', '3', '5', '8', '13', '20'];
+const Card = (cardValue: CardValue, setVote: (vote: CardValue) => void) =>
+  html`<div
+    className=${card}
+    onClick=${() => {
+      setVote(cardValue);
+    }}
+  >
+    ${cardValue}
+  </div>`;
 
-const votingPageStyle=css`
-    display: flex;
-    flex-wrap: wrap;
+const votingPageStyle = css`
+  display: flex;
+  flex-wrap: wrap;
 `;
-export const VotingPage = (values) =>
-  html`<div className=${votingPageStyle}>
-    ${CARD_VALUES.map(cardValue => Card(cardValue) )}
-    </div>`;
+export const VotingPage = () =>
+  html`<${WebSocketConsumer}
+    >${(value) =>
+      html`<div className=${votingPageStyle}>
+        ${CARD_VALUES.map((cardValue) => Card(cardValue, value.setVote))}
+      </div>`}<//
+  >`;
