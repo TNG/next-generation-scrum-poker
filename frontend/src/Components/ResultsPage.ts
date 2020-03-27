b9436ee338749ad7450301f08cd022fd12ce3945
Daniel Hetterich <danielh@Daniels-MacBook-Pro.local>
2020-03-27 14:06:31 +0100

    improved styling for results

--- frontend/src/Components/ResultsPage.ts
+++ frontend/src/Components/ResultsPage.ts
@@ -2,13 +2,41 @@ import { html } from '../html.js';
 import { connectToWebSocket, WebSocketConsumer } from './WebSocket.js';
 import { css } from '../css.js';
 import { WebSocketApi } from '../types/WebSocket.js';
+import { BORDER_RADIUS, TNG_BLUE, TNG_GRAY } from './LoginPage.js';
+
 
 const styling = css`
-  background: yellow;
-
-  .button {
-    color: red;
+  .table {
+    text-align: left;
+    padding: 15px;
   }
+  .td {
+    padding 25px;
+  }
+  .th {
+        color: darkblue;
+        padding 25px;
+  }
+  .button {
+    border: none;
+    color: white;
+    cursor: pointer;
+    background: ${TNG_BLUE};
+    ${BORDER_RADIUS}
+    height: 50px;
+    width: 150px;
+  }
+  .cardStyle = {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-width: 3px;
+      border-style: solid;
+      border-color: ${TNG_BLUE};
+      border-radius: 10px;
+      color: ${TNG_BLUE};
+      margin: 10px;
+   }
 `;
 
 const ProtoResultsPage = ({ socket }: { socket: WebSocketApi }) =>
@@ -19,9 +47,26 @@ const ProtoResultsPage = ({ socket }: { socket: WebSocketApi }) =>
         socket.resetVotes();
       }}
     >
-      We
+      Reset votes
     </button>
-    got: ${JSON.stringify(socket.state)}
-  </div>`;
+    <div className="cardStyle">
+    <table className="table">
+        <tr className="th"> <th>Name</th> <th>Vote</th> </tr>
+        ${socket.state.votes.map( (userAndVote) => {
+            const userName = Object.keys(userAndVote)[0];
+            const userVote = userAndVote[userName];
+            return html`
+                  <tr>
+                      <td>${Object.keys(userAndVote)[0]} </td>
+                      <td>${Object.values(userAndVote)[0]} </td>
+                  </tr>`
+        }
+        )}
+    </table>
+    </div>
+</div>`
+
+
+
 
 export const ResultsPage = connectToWebSocket(ProtoResultsPage);
