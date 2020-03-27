bc538050162c21a46021bee77e0634e92427b690
Edgar Kellermann <edgar.kellermann@tngtech.com>
2020-03-27 10:48:55 +0100

    extract config and refactor voting page
    Signed-off-by: Edgar Kellermann <edgar.kellermann@tngtech.com>

--- frontend/src/Components/VotingPage.ts
+++ frontend/src/Components/VotingPage.ts
@@ -1,59 +1,9 @@
+import React from '../../node_modules/es-react/dev/react.js';
+import { CARD_VALUES } from '../config.js';
 import { css } from '../css.js';
 import { html } from '../html.js';
-import { CardValue } from '../types/WebSocket.js';
 import { BORDER_RADIUS, TNG_BLUE, TNG_GRAY } from './LoginPage.js';
 import { connectToWebSocket } from './WebSocket.js';
-
-const cardStyle = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-width: 3px;
-  border-style: solid;
-  border-color: ${TNG_BLUE};
-  border-radius: 10px;
-  color: ${TNG_BLUE};
-  height: 160px;
-  width: 100px;
-  margin: 10px;
-  :hover {
-    background: ${TNG_GRAY};
-    cursor: pointer;
-  }
-  :active {
-    background: ${TNG_BLUE};
-    color: white;
-  }
-`;
-const CARD_VALUES: Array<CardValue> = [
-  'coffee',
-  '0',
-  '0.5',
-  '1',
-  '2',
-  '3',
-  '5',
-  '8',
-  '13',
-  '20',
-  '40',
-  '100',
-];
-const Card = ({
-  cardValue,
-  setVote,
-}: {
-  cardValue: CardValue;
-  setVote: (vote: CardValue) => void;
-}) =>
-  html`<div
-    className=${cardStyle}
-    onClick=${() => {
-      setVote(cardValue);
-    }}
-  >
-    ${cardValue}
-  </div>`;
 
 const votingPageStyle = css`
   display: flex;
@@ -67,18 +17,42 @@ const votingPageStyle = css`
   }
   .card-collection {
     display: flex;
-    align-items: center;
+    justify-content: center;
     flex-direction: row;
     flex-wrap: wrap;
   }
-  .reveal-votes-button {
+  .card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-width: 3px;
+    border-style: solid;
+    border-color: ${TNG_BLUE};
+    border-radius: 10px;
+    color: ${TNG_BLUE};
+    height: 160px;
+    width: 100px;
+    margin: 10px;
+    :hover {
+      background: ${TNG_GRAY};
+      cursor: pointer;
+    }
+    :active {
+      background: ${TNG_BLUE};
+      color: white;
+    }
+  }
+  .button {
     border: none;
     color: white;
     cursor: pointer;
     background: ${TNG_BLUE};
-    ${BORDER_RADIUS}
+    ${BORDER_RADIUS};
     height: 50px;
     width: 150px;
+    :hover {
+      background: ${TNG_GRAY};
+    }
   }
 `;
 export const VotingPage = connectToWebSocket(
@@ -87,10 +61,17 @@ export const VotingPage = connectToWebSocket(
     <div className="card-collection">
       ${CARD_VALUES.map(
         (cardValue) =>
-          html`<${Card} cardValue=${cardValue} setVote=${socket.setVote} key=${cardValue}></${Card}>`
+          html`<div
+            className="card"
+            onClick=${() => {
+              socket.setVote(cardValue);
+            }}
+          >
+            ${cardValue}
+          </div>`
       )}
     </div>
-    <button className="reveal-votes-button" onClick=${() => socket.revealVotes()}>
+    <button className="button" onClick=${() => socket.revealVotes()}>
       Reveal Votes
     </button>
   </div>`
