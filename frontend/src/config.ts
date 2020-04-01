9a914bfeb313448414abbc4a5771137cf322729c
Andreas Hille <andreas.hille@tngtech.com>
2020-04-01 14:50:29 +0200

    fix rebase
    Signed-off-by: Andreas Hille <andreas.hille@tngtech.com>

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,6 @@ import { CardValue } from './types/WebSocket';
 export const WEBSOCKET_URL = 'wss://scrum-poker-backend.playground.aws.tngtech.com';
 
 export const CARD_VALUES: Array<CardValue> = [
-  '?',
   'coffee',
   '?',
   '0',
