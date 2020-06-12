7c8dcab8919d779b3c6fdf20f487608013834d80
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-06-12 07:26:47 +0200

    Fix build and update dependencies
    Signed-off-by: Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -2,6 +2,8 @@ import { terser } from 'rollup-plugin-terser';
 import { cleanOutputFolder } from './build-plugins/clean-output-folder.js';
 import { emitAssetsFromFile } from './build-plugins/emit-assets-from-file.js';
 import html from '@open-wc/rollup-plugin-html';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonJs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'index.html',
@@ -16,5 +18,12 @@ export default {
     format: 'esm',
     entryFileNames: '[name]-[hash].js',
   },
-  plugins: [cleanOutputFolder(), html(), emitAssetsFromFile('build/assets.js'), terser()],
+  plugins: [
+    cleanOutputFolder(),
+    nodeResolve(),
+    commonJs({ include: '**/node_modules/**' }),
+    html(),
+    emitAssetsFromFile('build/assets.js'),
+    terser(),
+  ],
 };
