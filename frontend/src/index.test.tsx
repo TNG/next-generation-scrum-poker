c236882d942946f5d7d0cf708f4054d55ef2cf09
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-04-02 07:41:21 +0200

    Fix tests
    Signed-off-by: Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>

--- frontend/src/index.test.tsx
+++ frontend/src/index.test.tsx
@@ -1,9 +1,9 @@
 describe('the entry point', () => {
-  it('displays the app as loading initially', () => {
+  it('displays the login screen initially', () => {
     jest.isolateModules(() => {
       require('./index');
     });
-    expect(document.body).toHaveTextContent('Connecting...');
+    expect(document.body).toHaveTextContent('NEXT GENERATIONSCRUM POKER');
     expect(document.body.children).toHaveLength(1);
     document.body.firstChild!.remove();
   });
