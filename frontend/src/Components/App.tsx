fb145179d50b88483cec49e9eed8e366d9065b91
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-04-07 20:46:32 +0200

    Prevent hovering animation on mobile
    Signed-off-by: Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>

--- frontend/src/Components/App.tsx
+++ frontend/src/Components/App.tsx
@@ -1,11 +1,14 @@
 import classes from './App.module.css';
 import { PageSelector } from './PageSelector';
+import { TouchDetector } from './TouchDetector';
 import { WebSocketProvider } from './WebSocket';
 
 export const App = () => (
   <div className={classes.app}>
     <WebSocketProvider>
-      <PageSelector />
+      <TouchDetector>
+        <PageSelector />
+      </TouchDetector>
     </WebSocketProvider>
   </div>
 );
