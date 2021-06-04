a6c47552172baafd5d66ebef830477e80135cd42
Alberto Diaz <alberto.diazdorado@tngtech.com>
2021-06-04 19:50:46 +0200

    Enable user to unselect a card: * Send the "not-voted" value upon click on the selected card * Remove the CSS selector `.card:active`, which seemingly had no effect apart from breaking this feature (please point out if I am wrong here) * Replace the selector `.card:focus` by `.card:focus-visible`. We want to highlight cards on focus to enable tab navigation, but this had an undesired effect: after unselecting a card, the card would be focused, thus rendering it in light blue and providing a bad user experience: "Did I unselect the card or not?". The pseudo class "focus-visible" correctly determines if the card was selected via keyboard (in which case it should be focused) of via mouse (in which case it should NOT be focused) * Also remove unused import in RevealButton.test.tsx
    Signed-off-by: Alberto Diaz <alberto.diazdorado@tngtech.com>

--- frontend/src/Components/CardSelector.tsx
+++ frontend/src/Components/CardSelector.tsx
@@ -4,25 +4,31 @@ import classes from './CardSelector.module.css';
 import { IconCoffee } from './IconCoffee';
 import { connectToWebSocket } from './WebSocket';
 import { IconObserver } from './IconObserver';
-import { BUTTON_OBSERVER, VOTE_COFFEE, VOTE_OBSERVER } from '../constants';
+import { BUTTON_OBSERVER, VOTE_COFFEE, VOTE_NOTE_VOTED, VOTE_OBSERVER } from '../constants';
 
 const ProtoCardSelector = ({ socket }: { socket: WebSocketApi }) => {
   const selectedCard = socket.state.votes[socket.loginData.user];
+
   return (
     <>
       <div class={classes.cardCollection}>
-        {socket.state.scale.map((cardValue) => (
-          <button
-            key={cardValue}
-            class={classNames([
-              classes.largeCard,
-              { [classes.selected]: selectedCard === cardValue },
-            ])}
-            onClick={() => socket.setVote(cardValue)}
-          >
-            {cardValue === VOTE_COFFEE ? <IconCoffee /> : cardValue}
-          </button>
-        ))}
+        {socket.state.scale.map((cardValue) => {
+          return (
+            <button
+              key={cardValue}
+              class={selectedCard === cardValue ? classNames([classes.largeCard, classes.selected]) : classNames([classes.largeCard])}
+              onClick={() => {
+                if (selectedCard === cardValue) {
+                  socket.setVote(VOTE_NOTE_VOTED);
+                } else {
+                  socket.setVote(cardValue);
+                }
+              }}
+            >
+              {cardValue === VOTE_COFFEE ? <IconCoffee /> : cardValue}
+            </button>
+          );
+        })}
       </div>
       <button
         class={classNames([
