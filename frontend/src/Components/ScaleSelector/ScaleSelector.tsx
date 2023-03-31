f1035ea661e77c27aca51f95d93e5ba2c6101957
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2023-03-31 07:08:41 +0200

    Add keyboard handling
    Signed-off-by: Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>

--- frontend/src/Components/ScaleSelector/ScaleSelector.tsx
+++ frontend/src/Components/ScaleSelector/ScaleSelector.tsx
@@ -5,54 +5,101 @@ import sharedClasses from '../../styles.module.css';
 import { connectToWebSocket } from '../WebSocket/WebSocket';
 import classes from './ScaleSelector.module.css';
 
+const availableScales = Object.entries(SCALES);
+
 export const ScaleSelector = connectToWebSocket(({ socket: { connected, setScale } }) => {
   const [open, setOpen] = useState(false);
-  const toggle = () => {
-    setOpen(!open);
-  };
-  const scaleSelectorRef = useRef<HTMLDivElement>(null);
+  const [selected, setSelected] = useState(-1);
+  const selectionButtonRef = useRef<HTMLButtonElement>(null);
   const dropdownRef = useRef<HTMLUListElement>(null);
+
+  const close = () => {
+    setOpen(false);
+    setSelected(-1);
+  };
+
+  const toggle = () => {
+    if (open) {
+      close();
+    } else {
+      setOpen(true);
+    }
+  };
+
+  const handleBlur = ({ relatedTarget }: FocusEvent) => {
+    if (relatedTarget !== selectionButtonRef.current && relatedTarget !== dropdownRef.current) {
+      close();
+    }
+  };
+
+  const handleKeyboardNavigation = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'ArrowDown':
+        setSelected((selected + 1) % availableScales.length);
+        event.preventDefault();
+        break;
+      case 'ArrowUp':
+        setSelected((Math.max(selected, 0) + availableScales.length - 1) % availableScales.length);
+        event.preventDefault();
+        break;
+      case 'Enter':
+      case ' ':
+        // Prevent "enter" from triggering the button if the dropdown is open
+        event.preventDefault();
+        if (selected >= 0) {
+          close();
+          setScale(availableScales[selected][1].values);
+          selectionButtonRef.current?.focus();
+        }
+    }
+  };
 
   useEffect(() => {
     if (!open) return;
     if (!connected) {
-      setOpen(false);
+      close();
       return;
     }
     if (dropdownRef.current) {
       dropdownRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+      dropdownRef.current.focus();
     }
-
-    const handleDocumentClick = (event: MouseEvent) => {
-      if (
-        scaleSelectorRef.current &&
-        event.target instanceof HTMLElement &&
-        !isChildNode(event.target, scaleSelectorRef.current)
-      ) {
-        setOpen(false);
-      }
-    };
-    document.addEventListener('click', handleDocumentClick, true);
-    return () => {
-      document.removeEventListener('click', handleDocumentClick, true);
-    };
   }, [open, connected]);
 
   return (
-    <div class={classes.scaleSelector} ref={scaleSelectorRef}>
-      <button disabled={!connected} onClick={toggle} class={sharedClasses.button}>
+    <div class={classes.scaleSelector}>
+      <button
+        disabled={!connected}
+        onClick={toggle}
+        class={sharedClasses.button}
+        ref={selectionButtonRef}
+        onBlur={handleBlur}
+        aria-expanded={open}
+        aria-haspopup="listbox"
+      >
         {SELECT_CHANGE_SCALE}
       </button>
       {open ? (
-        <ul class={classes.dropDown} ref={dropdownRef}>
-          {Object.entries(SCALES).map(([id, { name }]) => (
+        <ul
+          class={classes.dropDown}
+          ref={dropdownRef}
+          tabIndex={0}
+          role="listbox"
+          onBlur={handleBlur}
+          onKeyDown={handleKeyboardNavigation}
+        >
+          {availableScales.map(([id, { name }], index) => (
             <li
               class={classes.dropDownItem}
               key={id}
+              role="option"
+              aria-selected={selected === index}
               onClick={() => {
-                toggle();
+                close();
                 setScale(SCALES[id as ScaleName].values);
               }}
+              onMouseMove={() => selected !== index && setSelected(index)}
+              onMouseLeave={() => selected === index && setSelected(-1)}
             >
               {name}
             </li>
@@ -62,19 +109,3 @@ export const ScaleSelector = connectToWebSocket(({ socket: { connected, setScale
     </div>
   );
 });
-
-function isChildNode(child: HTMLElement | null, ancestor: HTMLElement): boolean {
-  if (!child) {
-    return false;
-  }
-  if (child === ancestor) {
-    return true;
-  }
-  let element: ParentNode | null = child;
-  while ((element = element.parentNode)) {
-    if (element === ancestor) {
-      return true;
-    }
-  }
-  return false;
-}
