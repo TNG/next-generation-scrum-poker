94f7fc848d5775b5a1c4bfb68544231b3b353f6f
Andreas Hille <andreas.hille@tngtech.com>
2024-06-28 18:45:29 +0200

    adjustment to privacy notice and imprint
    Signed-off-by: Andreas Hille <andreas.hille@tngtech.com>

--- frontend/src/Components/LegalNoticeContainer/LegalNoticeContainer.test.tsx
+++ frontend/src/Components/LegalNoticeContainer/LegalNoticeContainer.test.tsx
@@ -4,15 +4,15 @@ import { LegalNoticeContainer } from './LegalNoticeContainer';
 describe('The LegalNoticeContainer', () => {
   it('displays and hides the privacy notice when clicked', () => {
     const { getByText, queryByText } = render(<LegalNoticeContainer />);
-    expect(queryByText('Datenschutzerklärung')).toBe(null);
-    expect(queryByText('Impressum')).toBe(null);
+    expect(queryByText('Company and Contact Information')).toBe(null);
+    expect(queryByText('Privacy Policy')).toBe(null);
 
     fireEvent.click(getByText('privacy notice & imprint'));
-    expect(getByText('Datenschutzerklärung')).toBeVisible();
-    expect(getByText('Impressum')).toBeVisible();
+    expect(getByText('Company and Contact Information')).toBeVisible();
+    expect(getByText('Privacy Policy')).toBeVisible();
 
     fireEvent.click(getByText('privacy notice & imprint'));
-    expect(queryByText('Datenschutzerklärung')).toBe(null);
-    expect(queryByText('Impressum')).toBe(null);
+    expect(queryByText('Company and Contact Information')).toBe(null);
+    expect(queryByText('Privacy Policy')).toBe(null);
   });
 });
